0fcde93292e776ef6e4a15a9080752eb4f765ac6
Kiet <huynhminhkiet@gmail.com>
2016-11-25 12:04:29 +0700

    up

--- app/src/main/java/com/example/minhkiet/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/minhkiet/retrofitdemo/MainActivity.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.minhkiet.retrofitdemo.model.City;
 import com.example.minhkiet.retrofitdemo.model.Contact;
 import com.example.minhkiet.retrofitdemo.model.ContactResponse;
+import com.example.minhkiet.retrofitdemo.model.StatusResponse;
 import com.example.minhkiet.retrofitdemo.service.APIService;
 
 import java.util.ArrayList;
@@ -20,7 +22,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnGetData;
+    private Button btnGetData, btnCreateData;
 
     private TextView tvResult;
 
@@ -39,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         tvResult = (TextView) findViewById(R.id.tv_result);
+
+        btnCreateData = (Button) findViewById(R.id.btn_create_data);
+        btnCreateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createData();
+            }
+        });
     }
 
     private void getData() {
@@ -46,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("http://api.androidhive.info/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
         APIService service = retrofit.create(APIService.class);
 
         Call<ContactResponse> call = service.getContactList();
@@ -55,13 +64,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<ContactResponse> call, Response<ContactResponse> response) {
                 ContactResponse contactResponse= response.body();
-
                 showResult((ArrayList<Contact>) contactResponse.getContacts());
             }
 
             @Override
             public void onFailure(Call<ContactResponse> call, Throwable t) {
-                Log.d(getClass().getSimpleName(), "fail");
+            }
+        });
+    }
+
+    private void createData() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://soccernetworkapi-humiki.rhcloud.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        APIService service = retrofit.create(APIService.class);
+
+        City city = new City("C44", "Nghe An");
+        Call<StatusResponse> call = service.createUser(city);
+        call.enqueue(new Callback<StatusResponse>() {
+            @Override
+            public void onResponse(Call<StatusResponse> call, Response<StatusResponse> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<StatusResponse> call, Throwable t) {
+
             }
         });
     }
--- app/src/main/java/com/example/minhkiet/retrofitdemo/service/APIService.java
+++ app/src/main/java/com/example/minhkiet/retrofitdemo/service/APIService.java
@@ -1,15 +1,22 @@
 package com.example.minhkiet.retrofitdemo.service;
 
+import com.example.minhkiet.retrofitdemo.model.City;
 import com.example.minhkiet.retrofitdemo.model.ContactResponse;
+import com.example.minhkiet.retrofitdemo.model.StatusResponse;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by minhkiet on 21/11/2016
  */
 
 public interface APIService {
-    @GET("contacts")
+    @GET("/contacts")
     Call<ContactResponse> getContactList();
+
+    @POST("/cities")
+    Call<StatusResponse> createUser(@Body City city);
 }
